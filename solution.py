8e46f691f7d8108cb5dc049f785c1442f518c914
Nikolai Kovch <nr3ijs@gmail.com>
2023-04-07 16:34:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 891686040.. # Ваш chat ID, не меняйте название переменной
+chat_id = 891686040 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
